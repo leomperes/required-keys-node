ef1300a69cddad00cc0b18ee5c6a11512d6d1ca4
Leo M <leomperes@belanton.com>
2016-04-25 18:49:26 -0400

    PROPEX-X add - Start index.js to test properties

--- index.js
+++ index.js
@@ -2,4 +2,20 @@
  * Created by Leo M. <leomperes@belanton.com> on 4/22/16.
  */
 
-var propertiesExist = require('./lib/PropertiesExist');
+// Var propertiesExist = require('./lib/PropertiesExist');
+var obj = { body: ['portal', 'name'] };
+
+function iterate(obj, stack) {
+  for (var property in obj) {
+    if (obj.hasOwnProperty(property)) {
+      if (typeof obj[property] == 'object') {
+        iterate(obj[property], stack + '.' + property);
+      } else {
+        console.log(property + ' - ' + obj[property]);
+        $('#output').append($('<div/>').text(stack + '.' + property))
+      }
+    }
+  }
+}
+
+iterate(obj, '');
