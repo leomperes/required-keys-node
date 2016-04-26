ec0bd403c8973960d1ad3480a0638f472cd7d257
Leo M <leomperes@belanton.com>
2016-04-26 19:12:29 -0400

    PROPEX-X feat - Create http request multi level object

--- index.js
+++ index.js
@@ -2,23 +2,87 @@
  * Created by Leo M. <leomperes@belanton.com> on 4/22/16.
  */
 
-var obj = { params: ['tenantId'], body: ['portal', 'name'] };
-
-function iterate(obj, stack) {
-  // Loop on all object var properties.
-  for (var property in obj) {
-    // If object has the properties.
-    if (obj.hasOwnProperty(property)) {
-      //
-      if (typeof obj[property] == 'object') {
-        iterate(obj[property], stack + '.' + property);
-      } else {
-        console.log(stack);
-        console.log(stack + '.' + property);
-        console.log(property + ' - ' + obj[property]);
-      }
+var httpRequestBody = {
+  "data": [
+    {
+      "id": 1,
+      "title": "My first Post",
+      "content": "Hello world!",
+      "comments": [
+        {
+          "id": 1001,
+          "description": "Body Goodies",
+          "comments": [
+            {
+              "id": 1101,
+              "description": "Test third level...",
+              "comments": [
+                {
+                  "id": 1201,
+                  "description": "Test fourth level...",
+                  "comments": []
+                }
+              ]
+            },
+            {
+              "id": 1102,
+              "description": "...",
+              "comments": []
+            }
+          ]
+        },
+        {
+          "id": 1002,
+          "description": "Healthy",
+          "comments": []
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "title": "I am here again!",
+      "content": "Once upon a time...",
+      "comments": [
+        {
+          "id": 1003,
+          "description": "Body Goodies",
+          "comments": []
+        },
+        {
+          "id": 1004,
+          "description": "Healthy",
+          "comments": [
+            {
+              "id": 1401,
+              "description": "Test third level...",
+              "comments": []
+            }
+          ]
+        }
+      ]
     }
-  }
-}
+  ]
+};
 
-iterate(obj, 'test');
+// var requiredKeys = { params: ['tenantId'], body: ['portal', 'name'] };
+
+// function iterate(httpRequestBody, stack) {
+//   // Loop on all object var properties.
+//   for (var property in httpRequestBody) {
+//     // If object has the properties.
+//     if (httpRequestBody.hasOwnProperty(property)) {
+//       //
+//       if (typeof httpRequestBody[property] == 'object') {
+//         iterate(httpRequestBody[property], stack + '.' + property);
+//       } else {
+//         console.log(stack);
+//         console.log(stack + '.' + property);
+//         console.log(property + ' - ' + httpRequestBody[property]);
+//       }
+//     }
+//   }
+// }
+//
+// iterate(requiredKeys, 'test');
+
+console.log(httpRequestBody);
