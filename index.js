407bfb731456492d3e679443d40f74bdd642ab6e
Leo M <leomperes@belanton.com>
2016-04-25 21:09:18 -0400

    PROPEX-X add - Remove unnecessary file and folder

--- index.js
+++ index.js
@@ -2,17 +2,19 @@
  * Created by Leo M. <leomperes@belanton.com> on 4/22/16.
  */
 
-// Var propertiesExist = require('./lib/PropertiesExist');
-var obj = { body: ['portal', 'name'] };
+var obj = { params: ['tenantId'], body: ['portal', 'name'] };
 
 function iterate(obj, stack) {
+  // Loop on all object var properties.
   for (var property in obj) {
+    // If object has the properties.
     if (obj.hasOwnProperty(property)) {
+      //
       if (typeof obj[property] == 'object') {
         iterate(obj[property], stack + '.' + property);
       } else {
         console.log(property + ' - ' + obj[property]);
-        $('#output').append($('<div/>').text(stack + '.' + property))
+        // $('#output').append($('<div/>').text(stack + '.' + property))
       }
     }
   }
