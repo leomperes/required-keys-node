8aa9095b4176d4c18b756b66f0fe430df72c881c
Leo M <leomperes@belanton.com>
2016-05-10 21:40:42 -0400

    PROPEX-8 refact,docs : Add comments for the new module

--- index.js
+++ index.js
@@ -1,43 +1,38 @@
 /**
- * Created by Leo M. <leomperes@belanton.com> on 4/22/16.
+ *
+ * @type {{check: module.exports.check}}
+ * @version 0.0.2
+ * @author Leo M. <leomperes@belanton.com>
+ * @since 4/22/16
  */
-
-// var request = {};
-// var keys = {};
-//
-//
-// function check() {
-//
-//   return '';
-// }
-
-/**
- * 
- * @param req
- * @param props
- * @returns {*}
- */
-function check(req, props) {
-
-  var result;
-
-  if (props) {
-    result = new Error('The "' + req + '" key is missing from request ' + req, 500);
-  } else {
-    result = new Error('The "' + req + '" key is missing from request ' + req, 400);
-  }
-
-  return result;
-
-}
-
 module.exports = {
 
-  // // Properties:
-  // request: request,
-  // keys: keys,
+  /**
+   * Check if <code>keys</code> are in <code>request</code>.
+   *
+   * @param {Object} request HTTP request to check its required keys.
+   * @param {Object} keys Keys to be check on <code>request</code> param.
+   * @param {string[]} keys.params Keys to be check on <code>request</code> param.
+   * @param {string[]} keys.body Keys to be check on <code>request</code> param.
+   * @example <caption>Example usage of keys.</caption>
+   * {
+   *    params: ['tenantId'],
+   *    body: ['portal','name']
+   * }
+   * @returns {*}
+   * @since 0.0.1
+   */
+  check: function(request, keys) {
+    /** @type {*} */
+    var result;
 
-  // Functions:
-  check: check
+    if (keys) {
+      result = new Error('The "' + req + '" key is missing from request ' + req, 500);
+    } else {
+      result = new Error('The "' + req + '" key is missing from request ' + req, 400);
+    }
+
+    return result;
+  }
 
 };
